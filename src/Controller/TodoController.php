4b71dcd74a89e31a8d682e98508a097e897c5146
Quiet0o <mikolajklosowski112@gmail.com>
2023-02-28 19:06:55 +0100

    saving data from TodoType to database

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,16 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Todo;
+use App\Form\TodoType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 class TodoController extends AbstractController
 {
     #[Route('/todo', name: 'app_todo')]
-    public function index(): Response
+    public function index(Request $request,EntityManagerInterface $em): Response
     {
+        $todo = new Todo();
+
+        $form = $this->createForm(TodoType::class, $todo);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $todo = $form->getData();
+            $em->persist($todo);
+            $em->flush();
+            $em->flush();
+
+            return $this->render('todo/index.html.twig', [
+                'form' => $form->createView(),
+                'controller_name' => 'TodoController',
+            ]);
+        }
+
         return $this->render('todo/index.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'TodoController',
         ]);
     }
--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,6 +15,7 @@ class TodoType extends AbstractType
         $builder
             ->add('text')
             ->add('date')
+            ->add('save', SubmitType::class)
         ;
     }
 
