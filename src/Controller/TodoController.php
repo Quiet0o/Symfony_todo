1bf0065072a3a136cd706d612a746c2ff8afe553
Quiet0o <mikolajklosowski112@gmail.com>
2023-03-01 23:32:39 +0100

    Saving form input to database and show it

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -9,10 +9,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
 class TodoController extends AbstractController
 {
     #[Route('/todo', name: 'app_todo')]
-    public function index(Request $request,EntityManagerInterface $em): Response
+    public function index(Request $request,EntityManagerInterface $em,ManagerRegistry $doctrine): Response
     {
         $todo = new Todo();
 
@@ -20,22 +22,27 @@ class TodoController extends AbstractController
 
         $form->handleRequest($request);
 
+        $repository = $doctrine->getRepository(Todo::class);
+        $todos = $repository->findAll();
+
+        if (!$todos) {
+            dump('No todos found');
+        }
+
+
         if ($form->isSubmitted() && $form->isValid()) {
 
             $todo = $form->getData();
-            $em->persist($todo);
-            $em->flush();
-            $em->flush();
+            $em -> persist($todo);
+            $em -> flush();
 
-            return $this->render('todo/index.html.twig', [
-                'form' => $form->createView(),
-                'controller_name' => 'TodoController',
-            ]);
+            return $this->redirect($request->getUri());
         }
 
         return $this->render('todo/index.html.twig', [
             'form' => $form->createView(),
             'controller_name' => 'TodoController',
+            'todos' => $todos
         ]);
     }
 }
--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Todo;
+use DateTimeInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +16,10 @@ class TodoType extends AbstractType
     {
         $builder
             ->add('text')
-            ->add('date')
+            ->add('date',DateType::class, [
+                'widget' => 'choice',
+                'input'  => 'datetime_immutable'
+            ])
             ->add('save', SubmitType::class)
         ;
     }
