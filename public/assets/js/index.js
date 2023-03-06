ad9695027dbdcdbfe4cb7821b8d89381e7cddba6
Quiet0o <mikolajklosowski112@gmail.com>
2023-03-06 23:34:38 +0100

    adding decoration to date

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,7 @@
 const complete = document.querySelectorAll(".fa-check")
-const text = document.querySelectorAll("p")
+const text = document.querySelectorAll(".todo-text")
 const todos = document.querySelectorAll(".todo")
+const todos_date = document.querySelectorAll(".todo-date")
 
 
 complete.forEach((com,id)=>{
@@ -8,6 +9,8 @@ complete.forEach((com,id)=>{
     com.addEventListener("click",()=>{
 
         text[id].classList.toggle("completed")
+        todos_date[id].classList.toggle("completed")
         todos[id].classList.toggle("todo-completed")
+
     })
 })
